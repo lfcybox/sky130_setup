cac83a0a938a07b6293498feea0e75976adf5321
yrrapt <yrrapt@gmail.com>
2020-12-23 20:12:33 +0100

    Remove ef_style that breaks install

--- install_pdk.sh
+++ install_pdk.sh
@@ -21,7 +21,6 @@ git clone git://opencircuitdesign.com/open_pdks
 cd open_pdks
 git checkout open_pdks-1.0
 mkdir -p $HOME/skywater/pdk/skywater130
-# ./configure --with-sky130-source=$HOME/skywater/skywater-pdk --with-sky130-local-path=$HOME/skywater/pdk/skywater130 --with-ef-style
 ./configure --with-sky130-source=$HOME/skywater/skywater-pdk --with-sky130-local-path=$HOME/skywater/pdk/skywater130
 cd sky130
 make
